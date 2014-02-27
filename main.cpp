1a536494ed1a7a64a629181f787d570191eb6cee
Ethan <ewalker15@yahoo.com>
2014-02-27 09:29:47 -0600

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*******************************************************************
+*   File: main.cpp                                                 *
+*   Name: Ethan Walker                                             *
+*   Project: sorting-comparison                                    *
+*   Creation Date: 2/27/2014                                       *
+*   Purpose: CPP file to run sorts in order to compare sort time   *
+*                                                                  *
+*******************************************************************/
+
+#include <iostream>
+
+int main()
+{
+
+	return 0;
+}
